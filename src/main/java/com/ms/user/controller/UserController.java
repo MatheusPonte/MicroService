4c7c81f2db67a40f1f72aa1416217dcea5a96830
matheusponte <matheuspontes894@outlook.com>
2024-08-20 12:50:32 -0300

    Fix: post API UserController

--- src/main/java/com/ms/user/controller/UserController.java
+++ src/main/java/com/ms/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.ms.user.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,17 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ms.user.dto.UserRecordDto;
 import com.ms.user.models.UserModel;
+import com.ms.user.service.UserService;
 
 import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
 
+    final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("/Users")
-    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto){
-        
-        
-        return ResponseEntity.status(HttpStatus.CREATED).body();
-        
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userRecordDto) {
+
+        var usuario = new UserModel();
+        BeanUtils.copyProperties(userRecordDto, usuario);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(usuario));
+
     }
 }
